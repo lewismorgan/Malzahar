f68c50774173ba669e3d12c6a1c7b3404f1763de
Lewis <1057112+lewismorgan@users.noreply.github.com>
2018-04-20 17:39:33 -0400

    More app structuring...

---
+++ common-jvm/src/main/kotlin/com/lewisjmorgan/malzahar/placeholder.kt
@@ -0,0 +1,5 @@
+package com.lewisjmorgan.malzahar
+
+/**
+ * Created by lewis on 4/20/18.
+ */
---
+++ desktop/src/main/kotlin/com/lewisjmorgan/malzahar/desktop/placeholder.kt
@@ -0,0 +1,5 @@
+package com.lewisjmorgan.malzahar.desktop
+
+/**
+ * Created by lewis on 4/20/18.
+ */
