054bd52468597750edf0c151f43c24c5d1896c16
Lewis <1057112+lewismorgan@users.noreply.github.com>
2018-04-20 22:51:05 -0400

    Create basic view for simple summoner details

--- desktop/src/main/kotlin/com/lewisjmorgan/malzahar/desktop/view/MainView.kt
+++ desktop/src/main/kotlin/com/lewisjmorgan/malzahar/desktop/view/MainView.kt
@@ -1,5 +1,6 @@
 package com.lewisjmorgan.malzahar.desktop.view
 
+import javafx.geometry.Pos
 import tornadofx.*
 
 /**
@@ -7,6 +8,34 @@ import tornadofx.*
  */
 class MainView : BaseView("Malzahar") {
   override val root = borderpane {
-
+    center {
+      padding = insets(5.0, 5.0, 5.0, 5.0)
+      vbox {
+        spacing = 5.0
+        hbox {
+          spacing = 5.0
+          alignment = Pos.CENTER
+          label("Summoner") {
+            alignment = Pos.CENTER_LEFT
+          }
+          textfield("Summoner Name")
+          button("Find")
+        }
+        hbox {
+          spacing = 25.0
+          alignment = Pos.CENTER
+          vbox {
+            spacing = 5.0
+            label("SummonerName")
+            label("SummonerLevel")
+          }
+          vbox {
+            spacing = 5.0
+            label("Last Played Champion")
+            label("LastPlayedChampion")
+          }
+        }
+      }
+    }
   }
 }
