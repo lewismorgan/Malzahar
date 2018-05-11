55740460d0f5abb4fe715ca856be1fe27ca54135
Lewis <lewismorgan1995@icloud.com>
2018-05-10 20:12:12 -0400

    Move summoner tests to own spek

--- common-jvm/src/test/kotlin/com/lewisjmorgan/malzahar/riot/RiotApiSpeck.kt
+++ common-jvm/src/test/kotlin/com/lewisjmorgan/malzahar/riot/RiotApiSpeck.kt
@@ -1,40 +1,10 @@
 package com.lewisjmorgan.malzahar.riot
 
-import com.lewisjmorgan.malzahar.riot.summoner.getSummoner
-import com.lewisjmorgan.malzahar.riot.summoner.getSummonerByAccount
-import com.lewisjmorgan.malzahar.riot.summoner.getSummonerByName
-import com.lewisjmorgan.malzahar.riot.test.fromTestApi
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
-import org.jetbrains.spek.api.dsl.on
-
 
 class RiotApiSpeck : Spek({
   describe("Riot Summoner API") {
-    val riotApi by memoized { RiotApi.fromTestApi() }
-    on("getting a summoner by name") {
-      val summoners = listOf("Reats", "A Fiddley Foe")
-      summoners.forEach {
-        it("returns summoner with name $it") {
-          riotApi.getSummonerByName(it).test()
-              .assertValue { summoner -> summoner.name == it }
-        }
-      }
-    }
-    on("getting a summoner by id") {
-      val id = 21661091L
-      it("returns a summoner with id $id") {
-        riotApi.getSummoner(id).test()
-            .assertValue { summoner -> summoner.id == id }
-      }
-    }
-    on("getting a summoner by account") {
-      val accountId = 35140718L
-      it("returns a summoner with account id $accountId") {
-        riotApi.getSummonerByAccount(accountId).test()
-            .assertValue { summoner -> summoner.accountId == accountId }
-      }
-    }
+    // TODO Tests for jsonResponseString using mocks
   }
 })
---
+++ common-jvm/src/test/kotlin/com/lewisjmorgan/malzahar/riot/summoner/SummonerApiSpeck.kt
@@ -0,0 +1,37 @@
+package com.lewisjmorgan.malzahar.riot.summoner
+
+import com.lewisjmorgan.malzahar.riot.RiotApi
+import com.lewisjmorgan.malzahar.riot.test.fromTestApi
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+/**
+ * Created by lewis on 5/10/18.
+ */
+class SummonerApiSpeck : Spek({
+  val riotApi by memoized { RiotApi.fromTestApi() }
+  on("getting a summoner by name") {
+    val summoners = listOf("Reats", "A Fiddley Foe")
+    summoners.forEach {
+      it("returns summoner with name $it") {
+        riotApi.getSummonerByName(it).test()
+            .assertValue { summoner -> summoner.name == it }
+      }
+    }
+  }
+  on("getting a summoner by id") {
+    val id = 21661091L
+    it("returns a summoner with id $id") {
+      riotApi.getSummoner(id).test()
+          .assertValue { summoner -> summoner.id == id }
+    }
+  }
+  on("getting a summoner by account") {
+    val accountId = 35140718L
+    it("returns a summoner with account id $accountId") {
+      riotApi.getSummonerByAccount(accountId).test()
+          .assertValue { summoner -> summoner.accountId == accountId }
+    }
+  }
+})
