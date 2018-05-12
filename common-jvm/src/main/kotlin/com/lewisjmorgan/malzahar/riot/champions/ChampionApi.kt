fa5ab028932574afc4773abd5b84d77e0286b048
Lewis <lewismorgan1995@icloud.com>
2018-05-12 19:13:01 -0400

    Add Champion-v3 tests and rename to reflect Riot API

---
+++ common-jvm/src/main/kotlin/com/lewisjmorgan/malzahar/riot/champions/ChampionApi.kt
@@ -0,0 +1,28 @@
+package com.lewisjmorgan.malzahar.riot.champions
+
+import com.beust.klaxon.Klaxon
+import com.lewisjmorgan.malzahar.riot.RiotApi
+import io.reactivex.Single
+
+/**
+ * Retrieve all champions.
+ * @receiver RiotApi.Companion
+ * @param freeToPlay Boolean
+ */
+fun RiotApi.getChampions(freeToPlay: Boolean): Single<List<ChampionDto>> {
+  return getJsonResponseString("platform/v3/champions", listOf("freeToPlay" to freeToPlay.toString())).map { result ->
+    Klaxon().parse<ChampionListDto>(result)!!.champions
+  }
+}
+
+/**
+ * Retrieve champion by ID.
+ * @receiver RiotApi
+ * @param id Long
+ * @return Single<ChampionDto>
+ */
+fun RiotApi.getChampion(id: Long): Single<ChampionDto> {
+  return getJsonResponseString("platform/v3/champions/$id").map { result ->
+    Klaxon().parse<ChampionDto>(result)
+  }
+}
--- common-jvm/src/main/kotlin/com/lewisjmorgan/malzahar/riot/champions/ChampionDto.kt
+++ common-jvm/src/main/kotlin/com/lewisjmorgan/malzahar/riot/champions/ChampionDto.kt
@@ -1,6 +1,5 @@
 package com.lewisjmorgan.malzahar.riot.champions
 
-// TODO Add variables for ChampionDto
-class ChampionDto {
-
-}
+data class ChampionDto(val id: Long, val freeToPlay: Boolean, val active: Boolean,
+                       val botMmEnabled: Boolean, val botEnabled: Boolean, val rankedPlayEnabled: Boolean)
+data class ChampionListDto(val champions: List<ChampionDto>)
