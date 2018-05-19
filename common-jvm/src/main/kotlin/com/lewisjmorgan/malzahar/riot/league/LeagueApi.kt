a2e1c8cee66fd7ae83aceff0954f7042deb52866
Lewis <lewismorgan1995@icloud.com>
2018-05-19 16:11:24 -0400

    Added league-v3 API

---
+++ common-jvm/src/main/kotlin/com/lewisjmorgan/malzahar/riot/league/LeagueApi.kt
@@ -0,0 +1,37 @@
+package com.lewisjmorgan.malzahar.riot.league
+
+import com.beust.klaxon.Klaxon
+import com.lewisjmorgan.malzahar.riot.RiotApi
+import io.reactivex.Single
+
+private const val BASE_PATH = "league/v3"
+
+/**
+ * Get the challenger league for given queue.
+ * @receiver RiotApi
+ * @param queue String
+ * @return Single<List<LeagueDto>>
+ */
+fun RiotApi.getChallengerLeagueByQueue(queue: String): Single<LeagueListDto> {
+  return getJsonResponseString("$BASE_PATH/challengerleagues/by-queue/$queue").map { result ->
+    Klaxon().parse<LeagueListDto>(result)
+  }
+}
+
+fun RiotApi.getLeagueById(leagueId: String): Single<LeagueListDto> {
+  return getJsonResponseString("$BASE_PATH/leagues/$leagueId").map { result ->
+    Klaxon().parse<LeagueListDto>(result)
+  }
+}
+
+fun RiotApi.getMasterLeagueByQueue(queue: String): Single<LeagueListDto> {
+  return getJsonResponseString("$BASE_PATH/masterleagues/by-queue/$queue").map { result ->
+    Klaxon().parse<LeagueListDto>(result)
+  }
+}
+
+fun RiotApi.getLeaguePositionsBySummoner(summonerId: Long): Single<List<LeaguePositionDto>> {
+  return getJsonResponseString("$BASE_PATH/positions/by-summoner/$summonerId").map { result ->
+    Klaxon().parseArray<LeaguePositionDto>(result)
+  }
+}
---
+++ common-jvm/src/test/kotlin/com/lewisjmorgan/malzahar/riot/league/LeagueApiSpek.kt
@@ -0,0 +1,52 @@
+package com.lewisjmorgan.malzahar.riot.league
+
+import com.lewisjmorgan.malzahar.riot.RiotApi
+import com.lewisjmorgan.malzahar.riot.test.fromTestApi
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+
+/**
+ * Created by lewis on 5/18/18.
+ */
+class LeagueApiSpek : Spek({
+  describe("League v3 API") {
+    val api by memoized { RiotApi.fromTestApi() }
+    val queueType = "RANKED_SOLO_5x5"
+    on("getting challenger leagues by queue") {
+      it("returns the challenger leagues for $queueType") {
+        api.getChallengerLeagueByQueue(queueType)
+            .test()
+            .assertComplete()
+            .assertValue { it.queue == queueType }
+      }
+    }
+    on("getting master league by queue") {
+      it("returns the master league for $queueType") {
+        api.getMasterLeagueByQueue(queueType)
+            .test()
+            .assertComplete()
+            .assertValue { it.queue == queueType }
+      }
+    }
+    on("getting a league by id") {
+      val leagueId = "6e77e2d0-03d8-11e8-9e8c-c81f66cf135e"
+      it("returns the league for the id $leagueId") {
+        api.getLeagueById(leagueId)
+            .test()
+            .assertComplete()
+            .assertValue { it.leagueId == leagueId }
+      }
+    }
+    on("getting league positions by summoner") {
+      val summonerId = 21661091L
+      it("returns the league positions for summoner with id $summonerId") {
+        api.getLeaguePositionsBySummoner(summonerId)
+            .test()
+            .assertComplete()
+            .assertValue { it.isNotEmpty() }
+      }
+    }
+  }
+})
